ac06a536e63a476a6bf16416ee65296bda48b015
minje <minai621@naver.com>
2021-09-25 13:11:10 +0900

    Nweet Register

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,32 @@
-const Home:React.FC = () => {return (<span>Home</span>)}
+import React, {useState} from "react";
+
+const Home:React.FC = () => {
+    const [nweet, setNweet] = useState<any>("");
+
+    const onSubmit = (event: any) => {
+      event.preventDefault();
+    };
+
+    const onChange = (event: any) => {
+        event.preventDefault();
+        const {
+            target: { value },
+        } = event;
+        setNweet(value);
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                value={nweet}
+                onChange={onChange}
+                type="text"
+                placeholder="What's on your mind?"
+                maxLength={120}
+            />
+            <input type="submit" value="Nweet"/>
+        </form>
+    )
+}
 
 export default Home;
